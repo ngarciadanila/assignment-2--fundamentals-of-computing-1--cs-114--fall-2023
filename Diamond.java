01228bcedafac521ecc45de0fbd23756f5e310b9
ngarciadanila <143655749+ngarciadanila@users.noreply.github.com>
2023-10-11 21:07:28 -0400

    make scanner for input

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+
+    int input, asterisks, spaces;
+    Scanner intScanner;
+
+    System.out.println("Please enter a number: ");
+    intScanner = new Scanner (System.in);
+    input = intScanner.nextInt();
+
   }
 }
